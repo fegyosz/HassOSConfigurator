3bd24543a25dcd676dbc1a88f5b0f463ad71bbda
fegyosz <48118823+fegyosz@users.noreply.github.com>
2022-01-15 21:51:37 +0100

    Update run.sh

--- Pi4EnableSerial/run.sh
+++ Pi4EnableSerial/run.sh
@@ -27,7 +27,7 @@ set +e
       fi
       echo "$(</tmp/$partition/cmdline.txt )"
       cp /tmp/$partition/cmdline.txt /tmp/$partition/cmdline.bak
-      echo "dwc_otg.lpm_enable=0">/tmp/$partition/cmdline.txt
+      echo "dwc_otg.lpm_enable=0 console=tty1">/tmp/$partition/cmdline.txt
       echo "$(</tmp/$partition/cmdline.txt )"
     else
       echo "no $partition config found"
